f818f1418d4afea32ce5338a280b20a2e29ef4f8
leonweber <leonweber@posteo.de>
2015-08-27 10:56:06 +0200

    Add method stubs and transform staticmethods into classmethods

--- cosa_file.py
+++ cosa_file.py
@@ -35,12 +35,12 @@ class CosaHTML(CosaResource):
         reached_events = False
 
         for elem in self.body:
-            found_ageclasses = CosaHTML._find_classes(elem,
+            found_ageclasses = self._find_classes(elem,
                                                       css_classes.AGE_CLASS)
             if found_ageclasses:
                 ageclass = found_ageclasses[0].findall('./a')[0].attrib['name']
 
-            found_eventnames = CosaHTML._find_classes(elem,
+            found_eventnames = self._find_classes(elem,
                                                       css_classes.EVENT_NAME)
             if found_eventnames:
                 reached_events = True
@@ -49,36 +49,37 @@ class CosaHTML(CosaResource):
                     fragment = [elem]
                     yield (
                         ageclass,
-                        CosaHTML._filter_non_event_fragments(event_fragment))
+                        self._filter_non_event_fragments(event_fragment))
                 else:
                     fragment = [elem]
             elif reached_events and elem.tag.lower() == 'table':
                 fragment.append(elem)
 
-        yield (ageclass, CosaHTML._filter_non_event_fragments(fragment))
+        yield (ageclass, self._filter_non_event_fragments(fragment))
 
     def _meeting_fragment(self):
         return self.body.getchildren()[0]
 
-    @staticmethod
-    def _build_from_event_fragment(event_fragment, age_class, meeting):
+    @classmethod
+    def _build_from_event_fragment(cls, event_fragment, age_class, meeting):
         description_fragment = next(
-            CosaHTML._event_description_fragments_in_event_fragment(
+            cls._event_description_fragments_in_event_fragment(
                 event_fragment))
-        result_fragments = next(CosaHTML._result_fragments_in_event_fragment(
+        result_fragments = next(cls._result_fragments_in_event_fragment(
             event_fragment))
 
-        event_name = CosaHTML._get_text(description_fragment,
+        event_name = cls._get_text(description_fragment,
                                         css_classes.EVENT_NAME)
-        event_date = CosaHTML._get_text(description_fragment,
+        event_date = cls._get_text(description_fragment,
                                         css_classes.EVENT_DAY)
-        event_type = CosaHTML._get_text(description_fragment,
+        event_type = cls._get_text(description_fragment,
                                         css_classes.EVENT_TYPE)
 
-        event = Event(event_name, CosaHTML._str_to_date(event_date))
-        results = [CosaHTML._build_from_result_fragment(result_fragment, meeting,
-                                                       age_class)
-                   for result_fragment in result_fragments]
+        event = Event(event_name, cls._str_to_date(event_date))
+        results = [
+            cls._build_result(result_fragment, meeting,
+                                                 age_class)
+            for result_fragment in result_fragments]
 
         event.eventtype = event_type
         event.ak = age_class
@@ -86,18 +87,23 @@ class CosaHTML(CosaResource):
 
         return event
 
-    @staticmethod
-    def _build_from_result_fragment(result_fragment, meeting, ageclass):
-        pass
+    @classmethod
+    def _build_result(cls, result_fragment, meeting, ageclass):
+        if cls.is_relay(result_fragment):
+            result = cls._build_relay_result(result_fragment, meeting, ageclass)
+        else:
+            result = cls._build_default_result(result_fragment, meeting, ageclass)
 
-    @staticmethod
-    def _build_athlete_from_result_fragment(result_fragment, meeting, ageclass):
-        result_text = lambda css_class: CosaHTML._get_text(result_fragment,
+        return result
+
+    @classmethod
+    def _build_athlete_from_result_fragment(cls, result_fragment, meeting, ageclass):
+        result_text = lambda css_class: cls._get_text(result_fragment,
                                                            css_class)
         athlete_name = result_text(css_classes.RESULT_ATHLETE_NAME)
         athlete_birthyear = result_text(css_classes.RESULT_ATHLETE_BIRTHYEAR)
         athlete_club = result_text(css_classes.RESULT_ATHLETE_CLUB)
-        athlete_sex = CosaHTML._sex_from_ageclass(ageclass)
+        athlete_sex = cls._sex_from_ageclass(ageclass)
 
         athlete = meeting.get_or_create_athlete(athlete_name, athlete_birthyear,
                                                 athlete_club)
@@ -105,21 +111,21 @@ class CosaHTML(CosaResource):
 
         return athlete
 
-    @staticmethod
-    def _event_description_fragments_in_event_fragment(event_fragment):
+    @classmethod
+    def _event_description_fragments_in_event_fragment(cls, event_fragment):
         for fragment in event_fragment:
-            if CosaHTML._find_classes(fragment, css_classes.EVENT_DESCRIPTION):
+            if cls._find_classes(fragment, css_classes.EVENT_DESCRIPTION):
                 yield fragment
 
-    @staticmethod
-    def _result_fragments_in_event_fragment(event_fragment):
+    @classmethod
+    def _result_fragments_in_event_fragment(cls, event_fragment):
         for fragment in event_fragment:
-            if CosaHTML._find_classes(fragment,
+            if cls._find_classes(fragment,
                                       css_classes.RESULT_ATHLETE_NAME):
                 yield fragment
 
-    @staticmethod
-    def _used_css_classes(elem):
+    @classmethod
+    def _used_css_classes(cls, elem):
         children = elem.getchildren()
         found_classes = [elem.attrib['class']] if 'class' in elem.attrib else []
 
@@ -128,41 +134,41 @@ class CosaHTML(CosaResource):
 
         else:
             return found_classes + reduce(lambda a, b: a + b,
-                                          [CosaHTML._used_css_classes(child) for
+                                          [cls._used_css_classes(child) for
                                            child in children])
 
-    @staticmethod
-    def _filter_non_event_fragments(fragments):
+    @classmethod
+    def _filter_non_event_fragments(cls, fragments):
         event_fragments = [fragment for fragment in fragments if
                            any(['blE' in css_class
                                 for css_class in
-                                CosaHTML._used_css_classes(fragment)])]
+                                cls._used_css_classes(fragment)])]
         return event_fragments
 
-    @staticmethod
-    def _find_classes(fragment, classes):
+    @classmethod
+    def _find_classes(cls, fragment, classes):
         found_elems = reduce(lambda a, b: a + b,
                              [fragment.find_class(css_class) for css_class in
                               classes])
         return found_elems
 
-    @staticmethod
-    def _get_text(fragment, css_classes):
+    @classmethod
+    def _get_text(cls, fragment, css_classes):
         elems = reduce(lambda a, b: a + b,
-                       [CosaHTML._find_classes(fragment_part, css_classes) for
+                       [cls._find_classes(fragment_part, css_classes) for
                         fragment_part in
                         fragment])
         texts = [elem.text for elem in elems]
         assert len(texts) <= 1, 'Selection should be unique.'
         return texts[0] if texts else ""
 
-    @staticmethod
-    def _str_to_date(string):
+    @classmethod
+    def _str_to_date(cls, string):
         split = string.split('.')
         return date(int(split[2]), int(split[1]), int(split[0]))
 
-    @staticmethod
-    def _sex_from_ageclass(ageclass):
+    @classmethod
+    def _sex_from_ageclass(cls, ageclass):
         is_male = any([re.match(male_re, ageclass)
                        for male_re in regexps.MALE_AGECLASS])
         is_female = any([re.match(male_re, ageclass)
@@ -171,3 +177,15 @@ class CosaHTML(CosaResource):
         assert xor(is_female, is_male), 'Age class must determine sex.'
 
         return sexes.FEMALE if is_female else sexes.MALE
+
+    @classmethod
+    def is_relay(cls, result_fragment):
+        pass
+
+    @classmethod
+    def _build_relay_result(cls, result_fragment, meeting, ageclass):
+        pass
+
+    @classmethod
+    def _build_default_result(cls, result_fragment, meeting, ageclass):
+        pass
